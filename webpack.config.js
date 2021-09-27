b80fc8517af33e0397e4000efcb427ea163b5c0b
FloralLife <ocard9611@naver.com>
2021-09-27 23:50:50 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	mode: 'development',
 	entry: {
-		index: './src/index.js'
+		vendor: '@babel/polyfill',
+		index: './src/index.js',
+		search: './src/search.js'
 	},
 	output: {
 		path: path.resolve(__dirname, 'public'),
@@ -12,18 +14,44 @@ module.exports = {
 	},
 	module: {
 		rules: [{
-			test: /\.css$/,
-			use: [
-				'style-loader',
-				'css-loader'
-			]
-		}]
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /(\.scss|\.sass)$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							esModule: false
+						}
+					},
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(png|jpg|svg)$/,
+				use: ['file-loader']
+			}
+		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
 			filename: './index.html',
 			chunks: ['index']
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/search.html',
+			filename: './search.html',
+			chunks: ['search']
 		})
 	],
 	devtool: 'source-map'
